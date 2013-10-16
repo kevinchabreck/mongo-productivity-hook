420d1bc25efb6c528e5b5d58abe45301dd3a3d1a
Kevin Chabreck <kevin.chabreck@gmail.com>
2013-10-15 23:41:33 -0400

    created commit object to be stored to mongo database

--- post-commit.py
+++ post-commit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import subprocess
+import subprocess, pymongo
 from datetime import datetime
 
 p = subprocess.Popen(["tail", "-1", ".git/logs/HEAD"], stdout=subprocess.PIPE)
@@ -8,14 +8,27 @@ out, err = p.communicate()
 hash = out.split(' ')[1]
 timestamp = out.split('> ')[1].split(' ')[0]
 
-print "hash of most recent commit: " + hash
-print "timestamp: '{}'".format(timestamp)
+#print "hash of most recent commit: " + hash
+#print "timestamp: '{}'".format(timestamp)
 print "time: {}".format(datetime.fromtimestamp(float(timestamp)))
 
 p = subprocess.Popen(["git", "diff-tree", "--numstat", hash], stdout=subprocess.PIPE)
 out, err = p.communicate()
 
-print "numstat for commit {}:".format(hash)
-print out.replace('\n', ' ').replace('\t', ' ').split(' ')
+print "numstat for commit {}:\n".format(hash)
+print out
 
-#1381295858
+lines = out.split('\n')
+for i in range(1, len(lines) - 1):
+	line = lines[i].split('\t')
+	insertions = line[0]
+	deletions = line[1]
+	file = line[2]
+	print("file: {} \n{} insertions, {} deletions".format(file, insertions, deletions))
+
+commit = { 	this.hash = hash
+			this.insertions = insertions
+			this.deletions = deletions
+			this.timestamp = timestamp
+		}
+#print out.replace('\n', ' ').replace('\t', ' ').split(' ')
